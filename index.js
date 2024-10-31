d8d6188bf3ceee5d61af7ed92bffaae3792ddb1e
karenaskew <askewk8@googlemail.com>
2024-10-31 18:20:13 +1000

    add click to music note 1

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName("note")[0].addEventListener('onmouseover', function(){
+    var mishu = document.getElementsByClassName("headMishu")[0]
+    note.style.color= 'black';
+})
+{
