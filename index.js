efdd79cb47a9b5a7ce6b1fb48cc25dcf02c636a8
karenaskew <askewk8@googlemail.com>
2024-10-31 18:20:31 +1000

    update note to mishu

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.getElementsByClassName("note")[0].addEventListener('onmouseover', function(){
     var mishu = document.getElementsByClassName("headMishu")[0]
-    note.style.color= 'black';
+    mishu.style.color= 'black';
 })
 {
